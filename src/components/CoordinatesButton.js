62083f0a129996fd3ff1e241d1f167fb46236f74
Taylor Williams <taylor@thewilliams.net>
2020-03-19 22:13:49 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -2,6 +2,9 @@
 import React from 'react'
 export default class CoordinatesButton extends React.Component {
 
+  getCoords = (event) => {
+    console.log([event.clientX, event.clientY])
+  }
   render() {
     return <button onclick={this.getcoords} />
   }
